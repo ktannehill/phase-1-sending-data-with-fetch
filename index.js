b56d09d68f31f39820cf0ed427772f47b9b2c0c7
Kat Tannehill <katetannehill@gmail.com>
2023-09-16 09:08:22 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+const submitData = function (userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then(userData => document.body.append(userData.id))
+    .catch(error => document.body.append(error.message))
+}
